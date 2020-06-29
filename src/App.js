0ee6d747245193a5ad0bcca00117f01c57b28bed
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-06-29 04:40:18 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+import ListBooks from './listBooks'
+import SearchBooks from './searchBooks'
+import {Route} from 'react-router-dom'
+
+class BooksApp extends React.Component {
+constructor(){
+  super();
+  this.state={
+    books : [],
+    shelf: '',
+  }
+}
+
+componentDidMount(){
+  BooksAPI.getAll().then(books =>  {this.setState({
+      books
+  })
+  console.log(this.state.books)
+}) 
+}
+
+detectChange = (book,value) => {
+  BooksAPI.update(book,value);
+  this.state.books.map(item => {
+    if(item.id === book.id){
+      item.shelf = value;
+    }
+  })
+
+  this.setState({
+    shelf:value
+  })
+}
+
+  render() {
+    return (
+      <div className="app">
+        <p>{this.state.text}</p>
+        <Route exact path='/'render = {() => (<ListBooks viewBooks = {this.state.books} changeShelf = {this.detectChange}/>)}/>
+        <Route exact path='/search'component={SearchBooks}/>
+      </div>
+    )
+  }
+}
+
+export default BooksApp
